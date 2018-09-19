5c823f5a903f11e5e01cae1e07a5ff3ac83b2bda
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-09-18 20:12:29 -0400

    added some drums for in between

--- sonic_hw1.rb
+++ sonic_hw1.rb
@@ -1,7 +1,7 @@
 use_bpm 110
 
-def football_game(quarters)
-  if quarters >= 3
+def football_game(first_quarter)
+  if first_quarter >= 3
     play chord(:E3, :minor7)
     sleep 2
     play chord(:A3, :major)
@@ -10,12 +10,42 @@ def football_game(quarters)
   else
     play chord(:E3, :minor)
     sleep 2
-    play chord(:A3, :major7)
+    play chord(:A3, :major)
     sleep 2
-    football_game(quarters + 1)
+    football_game(first_quarter + 1)
+  end
+end
+
+def band_show(first_half)
+  if first_half <= 2
+    sleep 0.5
+    sample :drum_cymbal_soft
+    sleep 0.5
+    sample :drum_cymbal_soft
+    sleep 0.5
+    sample :drum_cymbal_soft
+    sleep 0.5
+    sample :drum_cymbal_soft
+    sleep 0.5
+    sample :drum_cymbal_soft
+    sleep 0.5
+    sample :drum_cymbal_soft
+    sleep 0.5
+    sample :drum_cymbal_soft
+  else
+    sample :bass_hit_c
+    sleep 1
+    sample :bass_hit_c
+    sleep 1
+    sample :bass_hit_c
+    sleep 1
+    sample :bass_hit_c
+    sleep 1
+    band_show(first_half + 1)
   end
 end
 
 4.times  do
   play football_game(1)
+  play band_show(1)
 end
